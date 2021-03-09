888beb32e09badf3cc50aa3d89c3ef8a212b106d
hobbit-whole <80264493+hobbit-whole@users.noreply.github.com>
2021-03-09 00:29:13 -0700

    wrote unpack_player_dict function

---
+++ TTH Project 2/app.py
@@ -0,0 +1,27 @@
+import constants
+
+if __name__ == "__main__":
+    players = constants.PLAYERS
+    teams = constants.TEAMS
+
+    player_info = []  
+    def unpack_players_dic():
+        
+        for player in players:
+            player_info.append(player)
+
+            for k, v in player.items():
+                if(k == 'height'):   
+                    player['height'] = int(v[0:2])
+
+                if(k == 'experience'):
+                    if(v == 'YES'):
+                        v.replace('YES', 'True')
+                        v = bool(v)
+                    else:
+                        v.replace('NO', 'False')
+                        v = bool(v)
+            
+            
+
+        unpack_players_dic()
