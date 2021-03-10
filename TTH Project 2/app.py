732b05cf65e103ae89b39293dad220ff9337e7b0
hobbit-whole <80264493+hobbit-whole@users.noreply.github.com>
2021-03-10 11:17:11 -0700

    add sort method

--- TTH Project 2/app.py
+++ TTH Project 2/app.py
@@ -1,48 +1,80 @@
-import constants
+from constants import PLAYERS, TEAMS
+from collections import defaultdict
 import random
 
-if __name__ == "__main__":
-    players = constants.PLAYERS
-    teams = constants.TEAMS
 
-    #print(players)
+if __name__ == "__main__":
     player_info = []
     team_info = []
-    
-    for player in players:
+
+    for player in PLAYERS:
         player_info.append(player)
 
-    for team in teams:
+    for team in TEAMS:
         team_info.append(team)
 
 
-def unpack_players_dic():
-
-
-    for k, v in player.items():
-        if(k == 'height'):   
-            player['height'] = int(v[0:2])
-
-        if(k == 'experience'):
-            if(v == 'YES'):
-                player['experience'] = True
-            else:
-                player['experience'] = False
-
-
-def pack_player_roster():
+def unpack_player_name():
     names = [d['name'] for d in player_info if 'name' in d]
-    stacked = []
+    #print(names)
+    return names
 
-    random.shuffle(names)
-    shuffled_names = [names[i] for i in range(6)]
-    print([names[i] for i in range(6)])
+def unpack_player_height():
+    height = [d['height'] for d in player_info if 'height' in d]
+    high = []
+    for i in range(len(height)):
+        high.append(height[i][0:2])
+   # print(high)
+    return high
 
-    
+def unpack_player_experience():
+    experience = [d['experience'] for d in player_info if 'experience' in d]
+    value = []
+    for i in range(len(experience)): 
+        if(experience[i] == 'YES'):
+            experience[i] = 'True'
+            value.append(experience[i])
+        else:
+            experience[i] = 'False'
+            value.append(experience[i])
+    #print(value)
+    return(value)
 
 
-    
+def create_dict(name, height, value):
 
-    
+    roster = defaultdict(list)
+    for i in range(0,len(player_info)):
+        roster[i] = name[i] + ", " + height[i] + ", " + value[i]
+   # print(roster)
+    return roster
 
-pack_player_roster()
+
+def teams():
+    indie_team = {}
+    for i in range(len(team_info)):
+        indie_team[i] = team_info[i]
+    #print(indie_team)
+    return indie_team
+
+def random_sort(roster, team):
+    team_roster = defaultdict(list)
+
+    for j in range(0,len(team)):
+        team_roster[j].append(team[j])
+        for i in range(0,len(roster)):
+            team_roster[j].append(roster[i])
+    print(team_roster)
+                
+    #print(indie)
+    #print(team[j])
+    #print(indie)
+
+
+name = unpack_player_name()
+height = unpack_player_height()
+value = unpack_player_experience()
+roster = create_dict(name, height, value)
+
+team = teams()
+random_sort(roster, team)
