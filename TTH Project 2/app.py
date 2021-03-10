7f35eca20600d45916e10819298366b271c6e594
hobbit-whole <80264493+hobbit-whole@users.noreply.github.com>
2021-03-09 20:50:43 -0700

    Not sure what to do anymore

--- TTH Project 2/app.py
+++ TTH Project 2/app.py
@@ -1,23 +1,48 @@
 import constants
+import random
 
 if __name__ == "__main__":
     players = constants.PLAYERS
     teams = constants.TEAMS
 
+    #print(players)
     player_info = []
+    team_info = []
     
-    def unpack_players_dic():
-        for player in players:
-            player_info.append(player)
+    for player in players:
+        player_info.append(player)
 
-            for k, v in player.items():
-                if(k == 'height'):   
-                    player['height'] = int(v[0:2])
+    for team in teams:
+        team_info.append(team)
 
-                if(k == 'experience'):
-                    if(v == 'YES'):
-                        v.replace('YES', 'True')
-                        v = bool(v)
-                    else:
-                        v.replace('NO', 'False')
-                        v = bool(v)
+
+def unpack_players_dic():
+
+
+    for k, v in player.items():
+        if(k == 'height'):   
+            player['height'] = int(v[0:2])
+
+        if(k == 'experience'):
+            if(v == 'YES'):
+                player['experience'] = True
+            else:
+                player['experience'] = False
+
+
+def pack_player_roster():
+    names = [d['name'] for d in player_info if 'name' in d]
+    stacked = []
+
+    random.shuffle(names)
+    shuffled_names = [names[i] for i in range(6)]
+    print([names[i] for i in range(6)])
+
+    
+
+
+    
+
+    
+
+pack_player_roster()
